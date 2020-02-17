fcab3db64fb81ca3b0c0acff46a02a8ae0dd8c2c
pomminman <61137750+pomminman@users.noreply.github.com>
2020-02-17 18:01:20 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '34QTVLH+WTusbDSvxb5rZkuncmHp0ynJybNqx1uUmGkKPYzYfLOfOqTShTOQINiTI+m9zNkM1uuvZl1STi/5zn7glU/xS7LRBt4q/qwDb0GrNk1E+rI3dkIGRL0RMJS7WlCr7YS/BPoF+P6kzOvHrQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'kP5QT1uSwDQZi6B8zDLuoefL/Egqepm5Lgvf/TxniaTPQRrrxR8k96y6mis/Sr57cCnunkhy6kMR+Uz6lVnsjb+31Ii83a8ufWmpJeTOjSzSXftwz+qMLDvPpRpttIyqme1zR/Fpsy31kTdV31TpYQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '678a0c94f972596a7d8030a0672bda03';
+$channelSecret = 'cdce62b09b8e4e5f7394218efff77583';
 
-$pushID = 'Ubb4de6814c2af35dd49bcfdf7cedbed5';
+$pushID = 'U4c96bce643189f9bf807e7bb97ee1dd4';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
